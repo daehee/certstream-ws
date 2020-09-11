aad0258e087c0a4a7ffb741f74397e7cb005e94c
Daehee <hello@daehee.com>
2020-09-11 11:27:11 -0700

    initial v

---
+++ certstream.go
@@ -0,0 +1,137 @@
+package certstream
+
+import (
+    "context"
+    "io"
+    "sync"
+    "time"
+
+    "github.com/gobwas/ws"
+    "github.com/gobwas/ws/wsutil"
+    "github.com/pkg/errors"
+    "github.com/valyala/fastjson"
+    "go.uber.org/zap"
+)
+
+const (
+    addr = "wss://certstream.calidog.io"
+)
+
+const (
+    // Send pings to certstream server with this period to keep conn alive
+    pingPeriod = 15 * time.Second
+)
+
+var (
+    p fastjson.Parser
+    logger, _ = zap.NewDevelopment()
+    sugar = logger.Sugar()
+)
+
+func CertStreamEventStream() (chan *fastjson.Value, chan error) {
+    outputStream := make(chan *fastjson.Value)
+    errStream := make(chan error)
+
+    go func() {
+        for {
+            cl, err := newClient()
+            if err != nil {
+                errStream <- errors.Wrap(err, "error connecting to certstream, sleeping a few seconds and reconnecting...")
+                time.Sleep(5 * time.Second)
+                continue
+            }
+            sugar.Info("connected to certstream")
+
+            done := make(chan struct{})
+            go cl.startPing(done)
+
+            loop:
+            for {
+                messages, err := cl.receive()
+                if err != nil {
+                    switch err {
+                    case io.ErrUnexpectedEOF:
+                        // silent reconnect on EOF from certstream server
+                        break loop
+                    default:
+                        errStream <- err
+                        break loop
+                    }
+                }
+                if len(messages) == 0 {
+                    continue
+                }
+                // var v *fastjson.Value
+                for _, msg := range messages {
+                    switch msg.OpCode {
+                    case ws.OpText:
+                        v, err := p.Parse(string(msg.Payload))
+                        if err != nil {
+                            errStream <- err
+                        }
+                        if string(v.GetStringBytes("message_type")) == "certificate_update" {
+                            outputStream <- v
+                        }
+                    case ws.OpPong:
+                        sugar.Info("pong")
+                    default:
+                    }
+                }
+            }
+
+            close(done)
+            cl.conn.Close()
+        }
+    }()
+
+    return outputStream, errStream
+}
+
+type client struct {
+    io sync.Mutex
+    conn io.ReadWriteCloser
+}
+
+func newClient() (*client, error) {
+    ctx := context.Background()
+    conn, _, _, err := ws.Dial(ctx, addr)
+    if err != nil {
+        return nil, err
+    }
+    cl := &client{
+        conn: conn,
+    }
+
+    return cl, nil
+}
+
+func (cl *client) receive() ([]wsutil.Message, error) {
+    cl.io.Lock()
+    // r, err := wsutil.ReadServerText(cl.conn)
+    messages, err := wsutil.ReadServerMessage(cl.conn, nil)
+    cl.io.Unlock()
+    if err != nil {
+        return nil, err
+    }
+    return messages, nil
+}
+
+func (cl *client) startPing(done <-chan struct{}) {
+    ticker := time.NewTicker(pingPeriod)
+    defer ticker.Stop()
+    for {
+        select {
+        case <-ticker.C:
+            cl.io.Lock()
+            err := wsutil.WriteClientMessage(cl.conn, ws.OpPing, []byte{})
+            cl.io.Unlock()
+            if err != nil {
+                ticker.Reset(pingPeriod)
+                break
+            }
+            sugar.Info("ping")
+        case <-done:
+            return
+        }
+    }
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/daehee/certstream-ws
+
+go 1.15
+
+require (
+	github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.0.4
+	github.com/pkg/errors v0.8.1
+	github.com/valyala/fastjson v1.5.4
+	go.uber.org/zap v1.16.0
+	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009 // indirect
+)
